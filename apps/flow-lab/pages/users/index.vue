fc6e78b1eaa224bc27b2935daf58454d41fa17b6
Alex Bates <ajbates93@gmail.com>
2023-03-13 23:01:09 +0000

    feat: useFetch to pull through data coming from api which is coming from db

--- apps/flow-lab/pages/users/index.vue
+++ apps/flow-lab/pages/users/index.vue
@@ -1,13 +1,44 @@
 <template>
   <div>
     <h3 class="text-3xl font-bold">{{ $route.meta.title }}</h3>
+    <table>
+      <thead>
+        <th>ID</th>
+        <th>Created At</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Role</th>
+      </thead>
+      <tbody>
+        <tr v-for="user in users">
+          <td>{{ user.id }}</td>
+          <td>{{ user.created_at }}</td>
+          <td>{{ user.first_name }}</td>
+          <td>{{ user.last_name }}</td>
+          <td>{{ user.role }}</td>
+        </tr>
+      </tbody>
+    </table>
+    
   </div>
 </template>
 
 <script setup lang="ts">
 definePageMeta({
-  title: 'Dashboard'
+  title: 'Users'
 })
+
+type User = {
+  id: number,
+  created_at: Date,
+  first_name: string,
+  last_name: string,
+  role: number
+}
+
+const users = ref<User[] | null>([])
+users.value = await useFetch<User[]>('http://localhost:3001/user').data.value
+
 </script>
 
 <style scoped>
